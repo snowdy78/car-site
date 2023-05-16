8ef077acdc51d2094d4716f26cad1092c85bc950
themrleoner12@mail.ru <themrleoner12@mail.ru>
2023-05-17 02:11:40 +0700

    bla bla bla

---
+++ load_header_profile.js
@@ -0,0 +1,8 @@
+let i = {
+    was_logged: false,
+};
+sessionStorage.setItem("name", JSON.stringify(i));
+
+let header = document.getElementById('header');
+header.getElementsByClassName('element');
+
