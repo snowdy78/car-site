df32ae28bf733cb60f6656409ea56bc98b71b926
Snowdy <63316967+snowdy78@users.noreply.github.com>
2023-05-20 12:44:08 +0700

    fgjfgh

--- load_header_profile.js
+++ load_header_profile.js
@@ -27,10 +27,12 @@ function load_header_profile()
         if (unlogged_header != null) 
         {
             unlogged_header.id = "logged-header";   
+            unlogged_header.className = 'log-header-template-columns bg-d2';
             unlogged_header.appendChild(prof_case);
         }
         else 
         {
+            logged_header.className = 'log-header-template-columns bg-d2';
             logged_header.appendChild(prof_case);
         }
     }
@@ -52,13 +54,53 @@ function load_header_profile()
         if (logged_header != null) 
         {
             logged_header.id = 'no-log-header';
+            logged_header.className = 'no-log-header-template-columns bg-d2';
             logged_header.appendChild(log_case);
             logged_header.appendChild(reg_case);    
         }
         else {
+            unlogged_header.className = 'no-log-header-template-columns bg-d2';
             unlogged_header.appendChild(log_case);
             unlogged_header.appendChild(reg_case);
         }
     }
 
 }
+let nav = document.getElementById('nav-menu');
+nav.addEventListener('click', () => {
+    let viewmenu = document.getElementById('vm');
+    if (viewmenu.className === "no-display")
+    {
+        viewmenu.className = "display";
+        let grid = document.getElementById('vm-grid');
+        let was_logged = JSON.parse( sessionStorage.getItem('header-states'))["was_logged"];
+        console.log(was_logged);
+        if (was_logged)
+        {
+            grid.className = 'grid-lk';
+            let elems = grid.getElementsByClassName('elem');
+            elems[0].textContent = "Лич. кабинет";
+            elems[1].textContent = "История Бронирования";
+            elems[0].setAttribute('onclick', 
+                "window.location.href = 'profile.html';");
+            elems[1].setAttribute('onclick', 
+                "window.location.href = 'history.html';");
+        }
+        else 
+        {
+            grid.className = 'grid';
+            let elems = grid.getElementsByClassName('elem');
+            elems[0].textContent = "Вход";
+            
+            elems[1].textContent = "Регистрация";
+        }
+    }
+    else 
+    {
+        viewmenu.className = 'no-display';
+    }; 
+});
+//addEventListener("resize", () => {
+    //let viewmenu = document.getElementById('vm');
+    //viewmenu.style.display = '';
+//});
--- register.js
+++ register.js
@@ -1,7 +1,50 @@
-let reg_btn = document.getElementById('cac-button');
-reg_btn.addEventListener('click', () => {
-    let header_states = JSON.parse(sessionStorage.getItem('header-states'));
-    console.log();
-    header_states["was_logged"] = true;
-    sessionStorage.setItem('header-states', JSON.stringify(header_states));
+
+let registrasion_button = document.getElementById('cac-button');
+registrasion_button.addEventListener('click', () => {
+    let is_filled = false;
+    let reg_fields = [];
+    let reg_elements = document.getElementsByClassName('reg-grid');
+    if (reg_elements.length != 0)
+    {
+        for (let i = 0; i < reg_elements.length; i++)
+        {
+            let cells = reg_elements[i].getElementsByClassName('cell');
+            if (cells.length != 0)
+                is_filled = true;
+            for(let j = 0; j < cells.length; j++)
+            {
+                let reg_field = cells[j].getElementsByClassName('text-input')[0];
+                is_filled = is_filled && reg_field.value != "";
+                reg_fields.push(reg_field);
+            }
+        }
+    }
+    else 
+    {
+        let log_elements = document.getElementsByClassName('login-grid');
+        for (let i = 0; i < log_elements.length; i++)
+        {
+            let cells = log_elements[i].getElementsByClassName('cell');
+            if (cells.length != 0)
+                is_filled = true;
+            for(let j = 0; j < cells.length; j++)
+            {
+                let reg_field = cells[j].getElementsByClassName('text-input')[0];
+                is_filled = is_filled && reg_field.value != "";
+                reg_fields.push(reg_field);
+            }
+        }
+    }
+
+    if (is_filled) 
+    {
+        let header_states = JSON.parse(sessionStorage.getItem('header-states'));
+        header_states["was_logged"] = true;
+        sessionStorage.setItem('header-states', JSON.stringify(header_states));
+    }
+    else {
+        let fill_err = document.getElementById('err-state');
+        fill_err.style.display = 'block';
+    }
 });
+
